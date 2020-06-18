6daff87c6393ed4a81c73885919fe33a8dfb1a6a
Keith Starling <kstarling@rosettastone.com>
2020-06-17 21:00:13 -0400

    load time picker before date picker

--- lib/tui/calendar/rails/version.rb
+++ lib/tui/calendar/rails/version.rb
@@ -1,7 +1,7 @@
 module Tui
   module Calendar
     module Rails
-      VERSION = "1.12.13.2"
+      VERSION = "1.12.13.3"
     end
   end
 end
--- vendor/assets/javascripts/tui-calendar.js
+++ vendor/assets/javascripts/tui-calendar.js
@@ -1,4 +1,4 @@
 //= require tui-code-snippet/tui-code-snippet
-//= require tui-date-picker/tui-date-picker
 //= require tui-time-picker/tui-time-picker
+//= require tui-date-picker/tui-date-picker
 //= require tui-calendar/tui-calendar
